3f5a32d93ac9c33afdc74031ef3b5dcc310be0b4
Ghostblade <143203309+Ghostblade-opt@users.noreply.github.com>
2025-12-06 21:06:06 +0800

    fixed preview - ForgotPass

--- app/src/main/java/com/main/servetogether/ui/forgotpass/ForgotPass.kt
+++ app/src/main/java/com/main/servetogether/ui/forgotpass/ForgotPass.kt
@@ -1,8 +1,15 @@
 package com.main.servetogether.ui.forgotpass
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -17,8 +24,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.main.servetogether.ui.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -129,4 +138,10 @@ fun ForgotPass(navController: NavController) {
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ForgotPassPreview() {
+    ForgotPass(navController = rememberNavController())
 }
