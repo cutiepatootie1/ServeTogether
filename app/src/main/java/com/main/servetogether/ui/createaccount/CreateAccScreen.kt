9093fb41d85dc711ba230800bf6ef5647cd75142
Ghostblade <143203309+Ghostblade-opt@users.noreply.github.com>
2025-12-06 20:49:00 +0800

    fixed preview problem - another fucntion for preview

--- app/src/main/java/com/main/servetogether/ui/createaccount/CreateAccScreen.kt
+++ app/src/main/java/com/main/servetogether/ui/createaccount/CreateAccScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
@@ -31,22 +30,24 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.main.servetogether.R
+import androidx.navigation.compose.rememberNavController
 import com.main.servetogether.ui.login.RoundedCheckbox
 import com.main.servetogether.ui.theme.White
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -64,7 +65,6 @@ fun CreateAccScreen(navController: NavController) {
 
     //states
     val showWarningPop = remember { mutableStateOf(false) }
-    val scrollState = rememberScrollState()
     val dateFormatter = remember { SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()) }
     val openDialog = remember { mutableStateOf(false) }
     val datePickerState = rememberDatePickerState(
@@ -365,6 +365,12 @@ fun CreateAccScreen(navController: NavController) {
 
 
         }
+
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun CreateAccScreenPreview() {
+    CreateAccScreen(navController = rememberNavController())
+}
