c56bc962b696159d4c8f6e6bfe816732b896f1ef
Ghostblade <143203309+Ghostblade-opt@users.noreply.github.com>
2025-12-17 21:22:09 +0800

    changed minSDK so it can to my huwawei phone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.main.servetogether"
-        minSdk = 33
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
