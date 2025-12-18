e7c9252c441cb8cc9c0b07f48d3e89e7740dc8e3
Ghostblade <143203309+Ghostblade-opt@users.noreply.github.com>
2025-12-18 11:22:52 +0800

    added some implemtations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 }
